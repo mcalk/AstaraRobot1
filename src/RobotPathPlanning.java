aafca63d87a7eb563712289651ad25664b66d875
Alkimos Makaronidis <mcalk3@gmail.com>
2015-03-06 15:49:58 +0000

    index checking for neighbors

--- src/RobotPathPlanning.java
+++ src/RobotPathPlanning.java
@@ -87,14 +87,18 @@ public class RobotPathPlanning extends Application {
 
 		openSet.add(start);
 
-		//Square cameFrom;
+		// Square cameFrom;
 
 		start.setgScore(0);
 		start.setfScore(start.getgScore()
 				+ calculateShortestDistanceTo(start, finish)); // Check
 
 		while (!openSet.isEmpty()) {
-			Square current = getMinHeuristic(openSet);       //check make sure to update the square stored in playfield since current is a copy
+			Square current = getMinHeuristic(openSet); // check make sure to
+														// update the square
+														// stored in playfield
+														// since current is a
+														// copy
 			if (current.equals(finish)) {
 
 				return reconstructPath(current.getCameFrom(), finish);
@@ -108,10 +112,28 @@ public class RobotPathPlanning extends Application {
 			int x = current.getxPos();
 			int y = current.getyPos();
 
-			currentNeigbors.add(playfield[y + 1][x]);
-			currentNeigbors.add(playfield[y - 1][x]);
-			currentNeigbors.add(playfield[y][x + 1]); // check values of indexes
-			currentNeigbors.add(playfield[y][x - 1]);
+			if (y + 1 < m) {
+
+				currentNeigbors.add(playfield[y + 1][x]);
+
+			}
+
+			if (y - 1 >= 0) {
+
+				currentNeigbors.add(playfield[y - 1][x]); // check values of
+															// indexes
+
+			}
+			if (x + 1 < n) {
+
+				currentNeigbors.add(playfield[y][x + 1]);
+
+			}
+			if (x - 1 >= 0) {
+
+				currentNeigbors.add(playfield[y][x - 1]);
+
+			}
 
 			for (int i = 0; i < currentNeigbors.size(); i++) {
 
@@ -126,7 +148,11 @@ public class RobotPathPlanning extends Application {
 				if (!openSet.contains(neibourNode)
 						|| tentative < neibourNode.getgScore()) {
 					neibourNode.setCameFrom(current);
-					neibourNode.setgScore(tentative);                        //check make sure to update the square stored in playfield since current is a copy
+					neibourNode.setgScore(tentative); // check make sure to
+														// update the square
+														// stored in playfield
+														// since current is a
+														// copy
 					neibourNode.setfScore(neibourNode.getgScore()
 							+ calculateShortestDistanceTo(neibourNode, finish));
 					if (!openSet.contains(neibourNode)) {
@@ -136,7 +162,7 @@ public class RobotPathPlanning extends Application {
 
 			}
 		}
-		
+
 		return null;
 
 	}
