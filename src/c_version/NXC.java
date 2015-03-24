ca007bf23bcb200ba8734c4eed5b374876518f4f
mcalk <mcalk3@gmail.com>
2015-03-24 00:55:15 +0000

    Added Came from functionality

--- src/c_version/NXC.java
+++ src/c_version/NXC.java
@@ -1,6 +1,7 @@
 package c_version;
 
-public class NXC {
+public class NXC
+{
 	private static final int COLUMNS = 18;
 	private static final int ROWS = 9;
 	private static final int ARRAY_LENGTH = 400;
@@ -16,30 +17,20 @@ public class NXC {
 			{ 1, 0, 0, 0, 0, 0, 0, 0, 0 }, { 1, 0, 0, 0, 0, 0, 0, 0, 0 },
 			{ 3, 0, 0, 0, 0, 0, 0, 0, 0 } };
 
-	public static void main(String[] args) {
-		// int[][] mapToCheck = {{0,0},{1,1},{2,2},{3,3},{4,4}};
-		// System.out.println(mapToCheck.length);
-		//
-		// int[][] newMap = remove(mapToCheck, 4);
-		// for(int[] x:mapToCheck)
-		// {
-		// for(int num:x)
-		// {
-		// System.out.print(num);
-		// }
-		// System.out.println("");
-		// }
-		// System.out.println(newMap);
-
-		int[][] arrayToFollow = aStarAlgorithm(0, 0, 3, 3);
-		for (int[] row : arrayToFollow) {
-			if (row[0] != -1)
-				System.out.println(row[0] + " " + row[1]);
+	public static void main(String[] args)
+	{
+		int[][] arrayToFollow = aStarAlgorithm(0, 0,13, 3);
+		for (int[] row : arrayToFollow)
+		{
+			if(row[0] != -1)
+				System.out.println(row[0] + " " + row[1] + " , parrent nodes -> " + row[2] + " " + row[3]);
 		}
+		
 	}
 
 	public static int[][] aStarAlgorithm(int startX, int startY, int finishX,
-			int finishY) {
+			int finishY)
+	{
 		int[][] openSet = new int[ARRAY_LENGTH][2];
 		int openSetHead = -1;
 		int openSetTail = 0;
@@ -48,27 +39,42 @@ public class NXC {
 		int closedSetHead = 0;
 		int closedSetTail = 0;
 
-		int[][] listOfNodesToFollow = new int[ARRAY_LENGTH][2];
-		int listOfNodesPointer = 0;
+//		int[][] listOfNodesToFollow = new int[ARRAY_LENGTH][2];
+//		int listOfNodesPointer = 0;
+		
+		
+		int[][] cameFrom = new int[ARRAY_LENGTH][4];
+		int cameFromPointer = 0;
 
 		// TO-DO This are not initialised
 		int[][] gScoresMap = new int[COLUMNS][ROWS];
 		int[][] fScoresMap = new int[COLUMNS][ROWS];
 
 		// Initialising
-		for (int i = 0; i < ARRAY_LENGTH; i++) {
+		for (int i = 0; i < ARRAY_LENGTH; i++)
+		{
 			openSet[i][0] = -1;
 			openSet[i][1] = -1;
 			closedSet[i][0] = -1;
 			closedSet[i][1] = -1;
-			listOfNodesToFollow[i][0] = -1;
-			listOfNodesToFollow[i][1] = -1;
+//			listOfNodesToFollow[i][0] = -1;
+//			listOfNodesToFollow[i][1] = -1;
+//			listOfNodesToFollow[i][2] = -1;
+//			listOfNodesToFollow[i][3] = -1;
+			
+			cameFrom[i][0] = -1;
+			cameFrom[i][1] = -1;
+			cameFrom[i][2] = -1;
+			cameFrom[i][3] = -1;
 		}
 
 		// Adding nodes not accessible into closedSet!
-		for (int X = 0; X < COLUMNS; X++) {
-			for (int Y = 0; Y < ROWS; Y++) {
-				if (map[X][Y] == 0) {
+		for (int X = 0; X < COLUMNS; X++)
+		{
+			for (int Y = 0; Y < ROWS; Y++)
+			{
+				if ( map[X][Y] == 0 )
+				{
 					closedSet[closedSetTail][0] = X;
 					closedSet[closedSetTail][1] = Y;
 					closedSetTail++;
@@ -81,8 +87,11 @@ public class NXC {
 		openSet[openSetTail][1] = startY;
 		openSetTail++;
 		openSetHead++;
+		
 
-		while (openSetHead > -1) {
+		
+		while (openSetHead > -1)
+		{
 			int[] current = getMinHeuristic(openSet, fScoresMap, openSetHead,
 					openSetTail);
 
@@ -90,15 +99,18 @@ public class NXC {
 			int currentY = current[1];
 			int index = current[2];
 
-			if (currentX == finishX && currentY == finishY) {
+			if ( currentX == finishX && currentY == finishY )
+			{
 				// TO-DO
 				// Implement reconstructing the path
 				// listOfNodesToFollow = reconstructPath(current);
-				listOfNodesToFollow[listOfNodesPointer][0] = finishX;
-			    listOfNodesToFollow[listOfNodesPointer][1] = finishY;
-			    listOfNodesPointer++;
+				
+//				listOfNodesToFollow[listOfNodesPointer][0] = finishX;
+//				listOfNodesToFollow[listOfNodesPointer][1] = finishY;
+//				listOfNodesPointer++;
+				
 				System.out.println("We have reached the finish! YAAAAY");
-				return listOfNodesToFollow;
+				return cameFrom;
 			}
 
 			openSet = remove(openSet, index);
@@ -115,33 +127,44 @@ public class NXC {
 			// smaller than gScore of this neighbourNode
 
 			int[][] currentNeighbour = getNeighbours(currentX, currentY);
-			for (int i = 0; i < 4; i++) {
+			
+			for (int i = 0; i < 4; i++)
+			{
 				int tentative = 0;
 				int currentNeighbourX = currentNeighbour[i][0];
 				int currentNeighbourY = currentNeighbour[i][1];
-				if (currentNeighbourX == -1 || currentNeighbourY == -1)
+				if ( currentNeighbourX == -1 || currentNeighbourY == -1 )
 					continue;
-				else if (contains(closedSet, closedSetHead, closedSetTail,
-						currentNeighbourX, currentNeighbourY))
+				if ( contains(closedSet, closedSetHead, closedSetTail,
+						currentNeighbourX, currentNeighbourY) )
 					continue;
+				
 				// Could have used method calculateShortestDsitanceTo, but it's
 				// always equal to 1 in this case!
 				tentative = gScoresMap[currentX][currentY] + 1;
 
-				if (!contains(openSet, openSetHead, openSetTail,
+				if ( !contains(openSet, openSetHead, openSetTail,
 						currentNeighbourX, currentNeighbourY)
-						|| tentative < gScoresMap[currentNeighbourX][currentNeighbourY]) {
-					listOfNodesToFollow[listOfNodesPointer][0] = currentX;
-					listOfNodesToFollow[listOfNodesPointer][1] = currentY;
-					listOfNodesPointer++;
+						|| tentative < gScoresMap[currentNeighbourX][currentNeighbourY] )
+				{
+//					listOfNodesToFollow[listOfNodesPointer][0] = currentX;
+//					listOfNodesToFollow[listOfNodesPointer][1] = currentY;
+//					listOfNodesPointer++;
 
+					cameFrom[cameFromPointer][0] = currentNeighbourX;
+					cameFrom[cameFromPointer][1] = currentNeighbourY;
+					cameFrom[cameFromPointer][2] = currentX;
+					cameFrom[cameFromPointer][3] = currentY;
+					cameFromPointer++;
+					
 					gScoresMap[currentNeighbourX][currentNeighbourY] = tentative;
 					fScoresMap[currentNeighbourX][currentNeighbourY] = tentative
 							+ calculateShortestDistanceTo(currentNeighbourX,
 									currentNeighbourY, finishX, finishY);
 
-					if (!contains(openSet, openSetHead, openSetTail,
-							currentNeighbourX, currentNeighbourY)) {
+					if ( !contains(openSet, openSetHead, openSetTail,
+							currentNeighbourX, currentNeighbourY) )
+					{
 						openSet[openSetTail][0] = currentNeighbourX;
 						openSet[openSetTail][1] = currentNeighbourY;
 						openSetTail++;
@@ -169,12 +192,14 @@ public class NXC {
 	 *         the diagonal distance to the goal
 	 */
 	public static int calculateShortestDistanceTo(int startX, int startY,
-			int finishX, int finishY) {
+			int finishX, int finishY)
+	{
 		return Math.abs(startX - finishX) + Math.abs(startY - finishY);
 	}
 
 	public static int[] getMinHeuristic(int[][] list, int[][] fScoreMap,
-			int head, int tail) {
+			int head, int tail)
+	{
 		int[] arrayToReturn = new int[3];
 		int currX = list[head][0];
 		int currY = list[head][1];
@@ -185,12 +210,14 @@ public class NXC {
 		arrayToReturn[1] = currY;
 		arrayToReturn[2] = head;
 
-		for (int i = head + 1; i < tail; i++) {
+		for (int i = head + 1; i < tail; i++)
+		{
 			currX = list[i][0];
 			currY = list[i][1];
 			candidate = fScoreMap[currX][currY];
 
-			if (currentMin > candidate) {
+			if ( currentMin > candidate )
+			{
 				currentMin = candidate;
 				arrayToReturn[0] = currX;
 				arrayToReturn[1] = currY;
@@ -210,9 +237,11 @@ public class NXC {
 	 *            Row index to be removed.
 	 * @return
 	 */
-	public static int[][] remove(int[][] openSet, int indexToRemove) {
+	public static int[][] remove(int[][] openSet, int indexToRemove)
+	{
 		int[][] matrixToReturn = openSet;
-		for (int i = indexToRemove; i < ARRAY_LENGTH - 1; i++) {
+		for (int i = indexToRemove; i < ARRAY_LENGTH - 1; i++)
+		{
 			matrixToReturn[i][0] = openSet[i + 1][0];
 			matrixToReturn[i][1] = openSet[i + 1][1];
 		}
@@ -234,37 +263,50 @@ public class NXC {
 	 *         If current node doesn't have one or more of neighbours their
 	 *         coordinates are set to -1.
 	 */
-	public static int[][] getNeighbours(int x, int y) {
+	public static int[][] getNeighbours(int x, int y)
+	{
 		int[][] arrayToReturn = new int[4][2];
 		// Setting UP
-		if (y > 0) {
+		if ( y > 0 )
+		{
 			arrayToReturn[0][0] = x;
 			arrayToReturn[0][1] = y - 1;
-		} else {
+		}
+		else
+		{
 			arrayToReturn[0][0] = -1;
 			arrayToReturn[0][1] = -1;
 		}
 		// Setting RIGHT
-		if (x < COLUMNS - 1) {
+		if ( x < COLUMNS - 1 )
+		{
 			arrayToReturn[1][0] = x + 1;
 			arrayToReturn[1][1] = y;
-		} else {
+		}
+		else
+		{
 			arrayToReturn[1][0] = -1;
 			arrayToReturn[1][1] = -1;
 		}
 		// Setting DOWN
-		if (y < ROWS - 1) {
+		if ( y < ROWS - 1 )
+		{
 			arrayToReturn[2][0] = x;
 			arrayToReturn[2][1] = y + 1;
-		} else {
+		}
+		else
+		{
 			arrayToReturn[2][0] = -1;
 			arrayToReturn[2][1] = -1;
 		}
 		// Setting LEFT
-		if (x > 0) {
+		if ( x > 0 )
+		{
 			arrayToReturn[3][0] = x - 1;
 			arrayToReturn[3][1] = y;
-		} else {
+		}
+		else
+		{
 			arrayToReturn[3][0] = -1;
 			arrayToReturn[3][1] = -1;
 		}
@@ -272,9 +314,11 @@ public class NXC {
 	}
 
 	public static boolean contains(int[][] closedSet, int head, int tail,
-			int x, int y) {
-		for (int i = head; i < tail; i++) {
-			if (closedSet[i][0] == x && closedSet[i][1] == y)
+			int x, int y)
+	{
+		for (int i = head; i < tail; i++)
+		{
+			if ( closedSet[i][0] == x && closedSet[i][1] == y )
 				return true;
 		}
 		return false;
